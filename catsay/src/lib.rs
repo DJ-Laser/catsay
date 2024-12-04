968e7969703765ae4fffa39c3f23eb89774cc3b4
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-04 17:28:54 -0500

    save changes bc this branch aint about vite anmore

--- catsay/src/lib.rs
+++ catsay/src/lib.rs
@@ -4,12 +4,6 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-  alert(&format!("Hello,{}!", name));
+pub fn say(text: String) -> String {
+  return text;
 }
--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,3 +1,25 @@
-pub fn main() {
-  println!("Cat says hi")
+use std::env;
+use std::io::{self, stdin, stdout, Write};
+
+use catsay::say;
+
+const HELP_TEXT: &str = "\
+Catsay written by DJ_Laser
+Usage: catsay <text>
+";
+
+pub fn main() -> Result<(), io::Error> {
+  // Discard argv0 (process path)
+  let args = env::args().skip(1);
+  let mut handle = stdout().lock();
+
+  if args.len() == 0 {
+    handle.write(HELP_TEXT.as_bytes())?;
+    return Ok(());
+  }
+
+  let text: String = args.collect::<Vec<String>>().join(" ");
+  handle.write(say(text).as_bytes())?;
+
+  Ok(())
 }
