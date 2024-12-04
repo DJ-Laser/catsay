7a686fab1c0346423989d0181c15baef65c3ed7e
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-04 18:17:44 -0500

    add stdin support

--- catsay/src/lib.rs
+++ catsay/src/lib.rs
@@ -1,5 +1,4 @@
 mod web_term;
-use wasm_bindgen::prelude::*;
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
--- catsay/src/main.rs
+++ catsay/src/main.rs
@@ -1,25 +1,54 @@
 use std::env;
-use std::io::{self, stdin, stdout, Write};
+use std::io::{self, IsTerminal, Read, Write};
+use std::string::FromUtf8Error;
 
 use catsay::say;
+
+#[derive(Debug)]
+pub enum CliError {
+  IoError(io::Error),
+  FromUtf8Error(FromUtf8Error),
+}
+
+impl From<io::Error> for CliError {
+  fn from(error: io::Error) -> CliError {
+    CliError::IoError(error)
+  }
+}
+
+impl From<FromUtf8Error> for CliError {
+  fn from(error: FromUtf8Error) -> CliError {
+    CliError::FromUtf8Error(error)
+  }
+}
 
 const HELP_TEXT: &str = "\
 Catsay written by DJ_Laser
 Usage: catsay <text>
 ";
 
-pub fn main() -> Result<(), io::Error> {
+pub fn main() -> Result<(), CliError> {
   // Discard argv0 (process path)
   let args = env::args().skip(1);
-  let mut handle = stdout().lock();
+  let mut stdout = io::stdout().lock();
 
   if args.len() == 0 {
-    handle.write(HELP_TEXT.as_bytes())?;
+    if io::stdin().is_terminal() {
+      stdout.write(HELP_TEXT.as_bytes())?;
+      return Ok(());
+    }
+
+    let mut buf: Vec<u8> = Vec::new();
+    io::stdin().lock().read_to_end(&mut buf)?;
+
+    let text = String::from_utf8(buf)?;
+    stdout.write(say(text).as_bytes())?;
+
     return Ok(());
   }
 
   let text: String = args.collect::<Vec<String>>().join(" ");
-  handle.write(say(text).as_bytes())?;
+  stdout.write(say(text).as_bytes())?;
 
   Ok(())
 }
